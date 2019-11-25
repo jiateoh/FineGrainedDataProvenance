20f884779963467f91e9bffdced6416dd089f295
Muhammad Ali Gulzar <gulzarmuhammadali@gmail.com>
2019-11-25 15:48:24 -0800

    first commit

---
+++ src/main/scala/StdDev.scala
@@ -0,0 +1,28 @@
+/**
+  * Created by malig on 10/31/19.
+  */
+
+import org.apache.spark._
+import symbolicprimitives.SymString
+import symbolicprimitives.Utils
+
+object StdDev {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("wordCount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val input = sc.textFile("file.log")
+
+     val input_new =  Utils.setInputZip(input.zipWithIndex()) // << Rewrites inserted
+      .map(s => new SymString(s._1, s._2)) // << Rewrites Inserted
+    // s x s
+    val sum_of_squares = input_new.map(s => s.toInt).map( a => a * a ).reduce(_+_)
+    val sum = input.map(s => s.toInt).reduce(_+_)
+    val square_of_sum = sum * sum
+    val count =  input.count()
+    val std = (sum_of_squares/count) - (square_of_sum/(count*count))
+    println(std)
+    Utils.retrieveProvenance(std.getProvenance())
+  }
+
+}
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import symbolicprimitives.{SymInt, SymString, Utils}
+
+/**
+  * Created by malig on 10/31/19.
+  */
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("wordCount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val input = sc.textFile("file.log")
+
+    val input_new = Utils.setInputZip(input.zipWithIndex()) // << Rewrites inserted
+      .map(s => new SymString(s._1, s._2)) // << Rewrites Inserted
+
+    // s x s
+    val count = input_new.flatMap(s => s.split(' '))
+      .map(s => (s, new SymInt(1, s.getProvenance())))
+      .reduceByKey(_ + _)//.filter(s => s._1.getValue().contains("ali"))
+      .collect()
+
+    // Measuring Storage overhead
+    println(count.map(a => a._2.getProvenanceSize()).reduce(_+_) + " Bytes")
+    println(count.head)
+
+    // Getting Provenance here
+    Utils.retrieveProvenance(count.head._2.getProvenance())
+  }
+
+}
