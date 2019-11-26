d534754f7478faec697bccceba2c9d85d0d4b637
usama54321 <usama54321@gmail.com>
2019-11-26 09:19:27 -0800

    added basic poc transformer to wrap integer literals inside map with symint

---
+++ src/main/scala/transformers/LiteralTransformer.scala
@@ -0,0 +1,34 @@
+package main.transformers
+
+import scala.meta._
+
+class LiteralTransformer extends Transformer {
+  var in_func = false
+  var param : List[Term.Param] = null
+
+  override def apply(tree: Tree): Tree = tree match {
+
+    case q"$expr.$ename((..$params) => $expr2)" => {
+      param = params
+      if(ename.toString() == "map")
+        in_func = true
+
+      var expr2New = super.apply(expr2).asInstanceOf[Term]
+
+      in_func = false
+      q"$expr.$ename((..$params) => $expr2New)"
+    }
+
+    case Lit.Int(literal) => {
+      if(in_func) {
+        var x = Term.Name(param.head.toString().stripPrefix("(").stripSuffix(")"))
+        return q"new SymInt($literal, $x)"
+      }
+      q"literal"
+    }
+
+    case node => {
+      super.apply(node)
+    }
+  }
+}
