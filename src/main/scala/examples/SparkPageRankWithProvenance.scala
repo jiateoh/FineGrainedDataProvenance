b918fb98d3012244e2c5837a0fc8d74df4689e25
Jason Teoh <jiateoh@gmail.com>
2020-01-03 15:28:33 -0800

    Temp commit - trying to get PageRank provenance to work on local

--- src/main/scala/examples/SparkPageRankWithProvenance.scala
+++ src/main/scala/examples/SparkPageRankWithProvenance.scala
@@ -19,24 +19,25 @@
 package examples
 
 import org.apache.spark.sql.SparkSession
+import sparkwrapper.SparkContextWithDP
 
 /**
-  * Computes the PageRank of URLs from an input file. Input file should
-  * be in format of:
-  * URL         neighbor URL
-  * URL         neighbor URL
-  * URL         neighbor URL
-  * ...
-  * where URL and their neighbors are separated by space(s).
-  *
-  * This is an example implementation for learning how to use Spark. For more conventional use,
-  * please refer to org.apache.spark.graphx.lib.PageRank
-  *
-  * Example Usage:
-  * {{{
-  * bin/run-example SparkPageRank data/mllib/pagerank_data.txt 10
-  * }}}
-  */
+ * Computes the PageRank of URLs from an input file. Input file should
+ * be in format of:
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * ...
+ * where URL and their neighbors are separated by space(s).
+ *
+ * This is an example implementation for learning how to use Spark. For more conventional use,
+ * please refer to org.apache.spark.graphx.lib.PageRank
+ *
+ * Example Usage:
+ * {{{
+ * bin/run-example SparkPageRank data/mllib/pagerank_data.txt 10
+ * }}}
+ */
 object SparkPageRank {
   
   def showWarning() {
@@ -55,16 +56,16 @@ object SparkPageRank {
     
     showWarning()
     
-    println("Args:")
-    args.foreach(println)
-    
     val spark = SparkSession
-      .builder
-      //.appName("SparkPageRank")
-      .getOrCreate()
+                .builder
+                .appName("SparkPageRank")
+                .getOrCreate()
+    // jteoh: change to test with UDF-Unaware API
+    val sc = new SparkContextWithDP(spark.sparkContext)
     
     val iters = if (args.length > 1) args(1).toInt else 10
-    val lines = spark.read.textFile(args(0)).rdd
+    // val lines = spark.read.textFile(args(0)).rdd
+    val lines = sc.textFile(args(0))
     val links = lines.map{ s =>
       val parts = s.split("\\s+")
       (parts(0), parts(1))
@@ -72,22 +73,19 @@ object SparkPageRank {
     var ranks = links.mapValues(v => 1.0).setName("Ranks @ iteration 0")
     
     for (i <- 1 to iters) {
-      val oldRanks = ranks
       val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
         val size = urls.size
         urls.map(url => (url, rank / size))
       }
-      
-      // count to make sure it gets computed...
-      // jt: cache requires some force computation.
       ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _).setName(s"Ranks @ iteration " +
-                                                                               s"${i}").cache()
-      ranks.count()
-      oldRanks.unpersist(blocking = false)
+                                                                               s"${i}")
     }
+  
+    // Small update to extract only values from Trackers (may need to update our API instead?)
+    // val output = ranks.collect()
+    val output = ranks.collect().map(_.value)
     
-    val output = ranks.collect()
-    // output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
+    output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
     
     spark.stop()
   }
