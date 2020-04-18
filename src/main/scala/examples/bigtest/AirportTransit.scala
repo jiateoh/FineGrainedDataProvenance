98693f43c9462550a9d264331b3dd046a20b7fa6
Jason Teoh <jiateoh@gmail.com>
2020-04-18 00:05:01 -0700

    Update AirportTransit example with BigSiftUI version, adapted to provenance

--- src/main/scala/examples/bigtest/AirportTransit.scala
+++ src/main/scala/examples/bigtest/AirportTransit.scala
@@ -1,150 +1,76 @@
 package examples.bigtest
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import sparkwrapper.SparkContextWithDP
 
 /**
-  * Created by malig on 3/27/18.
-  * Copied from BigTest by jteoh on 4/15/20: https://github
-  * .com/maligulzar/BigTest/blob/JPF-integrated/benchmarks/src/subject/programs/AirportTransit.scala
+  * Created by ali on 7/20/17.
+  * Copied from BigSiftUI repo by jteoh on 4/16/20
+  * https://github.com/maligulzar/BigSiftUI/blob/master/src/airport/AirportTransitAnalysis.scala
+  * Logging and other miscellaneous bigsift-specific functionality is removed.
   */
-object AirportTransit {
-  
-  
+object AirportTransitAnalysis {
   def main(args: Array[String]): Unit = {
-    
-    val conf = new SparkConf()
-    conf.setMaster("local[*]")
-    conf.setAppName("Weather")
-    val data1 = Array(" , ,90A0,-0A0,",
-                      " , ,-0A0,-0A0,",
-                      "",
-                      " , , ,",
-                      "",
-                      " , ,",
-                      " , ,",
-                      " , , ,",
-                      " , ,",
-                      " , , ,",
-                      " , ,",
-                      " , , ,",
-                      " , ,-0A9,-0A0,",
-                      " , ,-0A0,00A0,")
-    
-    val startTime = System.currentTimeMillis();
-    val _sc = new SparkContext(conf)
-    val sc = new SparkContextWithDP(_sc)
-    for (i <- 0 to data1.length - 1) {
-      try {
-        
-        val map1 = sc.parallelize(Array(data1(i))).map { s =>
-          def getDiff(arr: String, dep: String): Int = {
-            val a_min = Integer.parseInt(arr.substring(3, 5))
-            val a_hr = Integer.parseInt(arr.substring(0, 2))
-            val d_min = Integer.parseInt(dep.substring(3, 5))
-            val d_hr = Integer.parseInt(dep.substring(0, 2))
-            
-            val arr_min = a_hr * 60 + a_min
-            val dep_min = d_hr * 60 + d_min
-            
-            
-            if (dep_min - arr_min < 0) {
-              return 24 * 60 + dep_min - arr_min
-            }
-            return dep_min - arr_min
-          }
-          
-          val tokens = s.split(",")
-          val arrival_hr = tokens(2).split(":")(0)
-          val diff = getDiff(tokens(2), tokens(3))
-          val airport = tokens(4)
-          (airport + arrival_hr, diff)
-        }
-        val fil = map1.filter { v =>
-          val t1 = v._1
-          val t2 = v._2
-          t2 < 45
-        }
-        val out = fil.reduceByKey(_ + _)
-        out.collect().foreach(println)
-      }
-      catch {
-        case e: Exception =>
-          e.printStackTrace()
-      }
+    //set up spark configuration
+    val sparkConf = new SparkConf()
+    var logFile = ""
+    var local = 500
+    if (args.length < 2) {
+      sparkConf.setMaster("local[6]")
+      sparkConf.setAppName("Airport Transit Time Analysis").set("spark.executor.memory", "2g")
+      logFile = "airportdata"
+    } else {
+      logFile = args(0)
+      local = args(1).toInt
+    } //set up spark context
+    val ctx = new SparkContext(sparkConf) //set up lineage context and start capture lineage
+    val scdp = new SparkContextWithDP(ctx)
+    val input = scdp.textFileProv(logFile)
+  
+    val map = input.map { s =>
+      val tokens = s.split(",")
+      val arrival_hr = tokens(2).split(":")(0)
+      val diff = getDiff(tokens(2), tokens(3))
+      val airport = tokens(4)
+      ((airport, arrival_hr), diff)
     }
+    val fil = map.filter { v =>
+      v._2 < 45
+    }
+    val out = fil.reduceByKey(_ + _)
     
-    println("Time: " + (System.currentTimeMillis() - startTime))
+    out.collectWithProvenance().foreach(println)
+  }
+  
+  def fun (input: RDD[String] ): RDD[(((String, String), Int))] ={
+    val map = input.map { s =>
+      val tokens = s.split(",")
+      val arrival_hr = tokens(2).split(":")(0)
+      val diff = getDiff(tokens(2), tokens(3))
+      val airport = tokens(4)
+      ((airport, arrival_hr), diff)
+    }
+    val fil = map.filter { v =>
+      v._2 < 45
+    }
+    val out = fil.reduceByKey(_ + _)
+    out
+  }
+  
+  def failure(record: (Any, Int)): Boolean = {
+    record.asInstanceOf[Tuple2[Any, Int]]._2 < 0
+  }
+  
+  
+  def getDiff(arr: String, dep: String): Int = {
+    val arr_min = arr.split(":")(0).toInt * 60 + arr.split(":")(1).toInt
+    val dep_min = dep.split(":")(0).toInt * 60 + dep.split(":")(1).toInt
+    // if(dep_min - arr_min < 0){
+    //  return 24*60 + dep_min - arr_min
+    // }
+    return dep_min - arr_min
   }
   
   
 }
-
-
-// val map1 = sc.textFile("/Users/malig/workspace/up_jpf/benchmarks/src/datasets/airportdata/part-00000").map { s =>
-
-/*
-*
- val text = sc.textFile("hdfs://scai01.cs.ucla.edu:9000/clash/datasets/bigsift/airport").sample(false, 1)
- text.cache()
- text.count()
- text.map { s =>
-      def getDiff(arr: String, dep: String): Int = {
-       val arr_min = Integer.parseInt(arr.split(":")(0)) * 60 + Integer.parseInt(arr.split(":")(1))
-        val dep_min = Integer.parseInt(dep.split(":")(0)) * 60 + Integer.parseInt(dep.split(":")(1))
-        if(dep_min - arr_min < 0){
-          return 24*60 + dep_min - arr_min
-        }
-        return dep_min - arr_min
-      }
-      val tokens = s.split(",")
-      val arrival_hr = tokens(2).split(":")(0)
-      val diff = getDiff(tokens(2), tokens(3))
-      val airport = tokens(4)
-      (airport+ arrival_hr, diff)}.filter { v =>
-      val t1  = v._1
-      val t2 = v._2
-      t2 < 45}.reduceByKey(_ + _).collect().foreach(println)
-       text.filter { s =>
-      def getDiff(arr: String, dep: String): Boolean = {
-       val arr_min = Integer.parseInt(arr.split(":")(0)) * 60 + Integer.parseInt(arr.split(":")(1))
-        val dep_min = Integer.parseInt(dep.split(":")(0)) * 60 + Integer.parseInt(dep.split(":")(1))
-        dep_min - arr_min < 0
-      }
-      val tokens = s.split(",")
-      val arrival_hr = tokens(2).split(":")(0)
-     getDiff(tokens(2), tokens(3))
-     }.count()
-            text.filter { s =>
-      def getDiff(arr: String, dep: String): Boolean = {
-       val arr_min = Integer.parseInt(arr.split(":")(0)) * 60 + Integer.parseInt(arr.split(":")(1))
-        val dep_min = Integer.parseInt(dep.split(":")(0)) * 60 + Integer.parseInt(dep.split(":")(1))
-        dep_min - arr_min >=0
-      }
-      val tokens = s.split(",")
-      val arrival_hr = tokens(2).split(":")(0)
-     getDiff(tokens(2), tokens(3))
-     }.count()
-      text.map { s =>
-      def getDiff(arr: String, dep: String): Int = {
-       val arr_min = Integer.parseInt(arr.split(":")(0)) * 60 + Integer.parseInt(arr.split(":")(1))
-        val dep_min = Integer.parseInt(dep.split(":")(0)) * 60 + Integer.parseInt(dep.split(":")(1))
-        if(dep_min - arr_min < 0){
-          return 24*60 + dep_min - arr_min
-        }
-        return dep_min - arr_min
-      }
-      val tokens = s.split(",")
-      val arrival_hr = tokens(2).split(":")(0)
-      val diff = getDiff(tokens(2), tokens(3))
-      val airport = tokens(4)
-      (airport+ arrival_hr, diff)}.filter { v =>
-      val t1  = v._1
-      val t2 = v._2
-      t2 > 45}.count()
-filter2> "",1
-map3> "1"
-reduceByKey1> {1,2,3,4}
-DAG >reduceByKey1-filter2:filter2-map3
-K_BOUND >2
-* */
