ca6855f4e90583494e2f49d0e571fe49c050d708
Jason Teoh <jiateoh@gmail.com>
2020-04-21 00:23:09 -0700

    Extend InfluenceMarker min/max functions to also support ties

--- src/main/scala/provenance/data/InfluenceMarker.scala
+++ src/main/scala/provenance/data/InfluenceMarker.scala
@@ -6,20 +6,25 @@ object InfluenceMarker extends Enumeration {
   val left, right, both = Value
   
   type InfluenceFn[T] = (T, T) => Value
-  /** a > b ? left : right
+  /** a > b ? left : (a == b) ? both : right
     * need to define this as a no-param = function so that callers don't need to use `MaxFn _`
     * syntax */
   def MaxFn[T : Ordering]: InfluenceFn[T] =
     (a: T, b: T) => if (a > b) left else right
   
+  def MaxWithTiesFn[T: Ordering]: InfluenceFn[T] =
+    (a: T, b: T) => if (a > b) left else if (a == b) both else right
+  
   def MinFn[T : Ordering]: InfluenceFn[T] = Invert(MaxFn)
+  
+  def MinWithTiesFn[T : Ordering]: InfluenceFn[T] = Invert(MaxWithTiesFn)
   
   def Invert[T](fn: (T, T) => Value): InfluenceFn[T] = {
     (a: T, b: T) => {
       val orig = fn(a, b)
       if (orig == left) right
       else if (orig == right) left
-      else throw new RuntimeException("Cannot negate influence for a 'both' marker")
+      else both
     }
   }
   
