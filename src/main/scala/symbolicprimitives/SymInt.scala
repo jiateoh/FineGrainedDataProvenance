eba926aa57749d897f0434a6343c6ce6feac8f06
Jason Teoh <jiateoh@gmail.com>
2020-04-21 00:25:14 -0700

    Add SymImplicit conversions for common numeric tuples

--- src/main/scala/symbolicprimitives/SymInt.scala
+++ src/main/scala/symbolicprimitives/SymInt.scala
@@ -8,10 +8,6 @@ import provenance.data.Provenance
 
 object SymImplicits {
 
-  implicit def symInt2Int(s: SymInt): Int = s.getValue()
-  implicit def symFloat2Float(s: SymFloat): Float = s.getValue()
-  implicit def symDouble2Double(s: SymDouble): Double = s.getValue()
-
   //TODO: Using zero as default provenance here. We need to chain this disconnect through dependency analysis
 
   implicit def int2SymInt(s: Int): SymInt = new SymInt(s, Provenance.provenanceFactory.create(-1))
@@ -26,6 +22,39 @@ object SymImplicits {
   implicit def symFloat2SymInt(s: SymFloat): SymInt = new SymInt(s.getValue().toInt , s.getProvenance())
   implicit def symFloat2SymDouble(s: SymFloat): SymDouble = new SymDouble(s.getValue().toDouble , s.getProvenance())
   implicit def symInt2SymDouble(s: SymInt): SymDouble = new SymDouble(s.getValue().toDouble , s.getProvenance())
+
+  // A few common tuple options - these implicitly rely on the conversions defined above.
+  type SymLong = SymInt //TODO we don't have a SymLong type yet, so for simplicity we use SymInt. This is *not* accurate.
+  implicit def long2SymLong(s: Long): SymLong = new SymInt(s.toInt, Provenance.provenanceFactory.create(-1))
+  
+  // There are 16 definitions for pairs: 4 x 4.
+  implicit def intIntTupleToSyms(tuple: (Int, Int)): (SymInt, SymInt) = (tuple._1, tuple._2)
+  implicit def intLongTupleToSyms(tuple: (Int, Long)): (SymInt, SymLong) = (tuple._1, tuple._2)
+  implicit def intDoubleTupleToSyms(tuple: (Int, Double)): (SymInt, SymDouble) = (tuple._1, tuple._2)
+  implicit def intFloatTupleToSyms(tuple: (Int, Float)): (SymInt, SymFloat) = (tuple._1, tuple._2)
+  
+  implicit def longIntTupleToSyms(tuple: (Long, Int)): (SymLong, SymInt) = (tuple._1, tuple._2)
+  implicit def longLongTupleToSyms(tuple: (Long, Long)): (SymLong, SymLong) = (tuple._1, tuple._2)
+  implicit def longDoubleTupleToSyms(tuple: (Long, Double)): (SymLong, SymDouble) = (tuple._1, tuple._2)
+  implicit def longFloatTupleToSyms(tuple: (Long, Float)): (SymLong, SymFloat) = (tuple._1, tuple._2)
+  
+  implicit def doubleIntTupleToSyms(tuple: (Double, Int)): (SymDouble, SymInt) = (tuple._1, tuple._2)
+  implicit def doubleLongTupleToSyms(tuple: (Double, Long)): (SymDouble, SymLong) = (tuple._1, tuple._2)
+  implicit def doubleDoubleTupleToSyms(tuple: (Double, Double)): (SymDouble, SymDouble) = (tuple._1, tuple._2)
+  implicit def doubleFloatTupleToSyms(tuple: (Double, Float)): (SymDouble, SymFloat) = (tuple._1, tuple._2)
+  
+  implicit def floatIntTupleToSyms(tuple: (Float, Int)): (SymFloat, SymInt) = (tuple._1, tuple._2)
+  implicit def floatLongTupleToSyms(tuple: (Float, Long)): (SymFloat, SymLong) = (tuple._1, tuple._2)
+  implicit def floatDoubleTupleToSyms(tuple: (Float, Double)): (SymFloat, SymDouble) = (tuple._1, tuple._2)
+  implicit def floatFloatTupleToSyms(tuple: (Float, Float)): (SymFloat, SymFloat) = (tuple._1, tuple._2)
+  
+  
+  
+  // Implicits are applied in order of priority, so these should be defined last so we try to use
+  // symbolics as much as possible.
+  implicit def symInt2Int(s: SymInt): Int = s.getValue()
+  implicit def symFloat2Float(s: SymFloat): Float = s.getValue()
+  implicit def symDouble2Double(s: SymDouble): Double = s.getValue()
 }
 
 case class SymInt(value: Int, p : Provenance) extends SymBase(p) {
