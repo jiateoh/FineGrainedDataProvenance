9ff296f3f92680b01a4894bad35caba23542958d
Jason Teoh <jiateoh@gmail.com>
2020-04-20 21:59:09 -0700

    Add AirportTransit symbolic example

---
+++ src/main/scala/examples/benchmarks/symbolic_benchmarks/AirportTransitSymbolic.scala
@@ -0,0 +1,59 @@
+package examples.benchmarks.symbolic_benchmarks
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import sparkwrapper.SparkContextWithDP
+import symbolicprimitives.{SymInt, SymString, Utils}
+
+/**
+  * Created by ali on 7/20/17.
+  * Copied from BigSiftUI repo by jteoh on 4/16/20
+  * https://github.com/maligulzar/BigSiftUI/blob/master/src/airport/AirportTransitAnalysis.scala
+  * Logging and other miscellaneous bigsift-specific functionality is removed.
+  */
+object AirportTransitSymbolic {
+  def main(args: Array[String]): Unit = {
+    //set up spark configuration
+    val sparkConf = new SparkConf()
+    var logFile = ""
+    var local = 500
+    if (args.length < 2) {
+      sparkConf.setMaster("local[6]")
+      sparkConf.setAppName("Airport Transit Time Analysis").set("spark.executor.memory", "2g")
+      logFile = "datasets/airportdata"
+    } else {
+      logFile = args(0)
+      local = args(1).toInt
+    } //set up spark context
+    val ctx = new SparkContext(sparkConf) //set up lineage context and start capture lineage
+    val scdp = new SparkContextWithDP(ctx)
+    val input = scdp.textFileSymbolic(logFile)
+    Utils.setUDFAwareDefaultValue(true)
+    
+    val map = input.map { s =>
+      val tokens = s.split(",")
+      val arrival_hr = tokens(2).split(":")(0)
+      val diff = getDiff(tokens(2), tokens(3))
+      val airport = tokens(4)
+      ((airport, arrival_hr), diff)
+    }
+    val fil = map.filter { v =>
+      v._2 < 45
+    }
+    // TODO: define some sort of influence function
+    val out = fil.reduceByKey(_ + _)
+    
+    out.collectWithProvenance().foreach(println)
+  }
+  
+  def getDiff(arr: SymString, dep: SymString): SymInt = {
+    val arr_min = arr.split(":")(0).toInt * 60 + arr.split(":")(1).toInt
+    val dep_min = dep.split(":")(0).toInt * 60 + dep.split(":")(1).toInt
+    // if(dep_min - arr_min < 0){
+    //  return 24*60 + dep_min - arr_min
+    // }
+    return dep_min - arr_min
+  }
+  
+  
+}
