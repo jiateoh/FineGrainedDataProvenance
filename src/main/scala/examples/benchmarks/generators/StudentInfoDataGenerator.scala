f7b9de5e614bc771ff4ac677715eac083589f458
Muhammad Ali Gulzar <gulzarmuhammadali@gmail.com>
2020-05-11 00:52:12 -0700

    updates on studentinfo data generator

--- src/main/scala/examples/benchmarks/generators/StudentInfoDataGenerator.scala
+++ src/main/scala/examples/benchmarks/generators/StudentInfoDataGenerator.scala
@@ -12,7 +12,7 @@ object StudentInfoDataGenerator {
   var logFile = ""
   var partitions = 10
   var dataper  = 2500000
-  var fault_rate = 0.000001
+  var fault_rate = 0.000004
   val random = new Random(42)
   def shouldInjectFault(grade: String): Boolean = {
     grade == "Junior" && random.nextDouble() <= fault_rate
@@ -42,15 +42,16 @@ object StudentInfoDataGenerator {
     val sc = new SparkContext(sparkConf)
     sc.parallelize(Seq[Int]() , partitions).mapPartitions { _ =>
       (1 to dataper).map { _ =>
-
-
         val studentId = random.nextInt(99999999)
         val major = majorList(random.nextInt(majorList.length))
-        val insert_fault = shouldInjectFault(major)
+
         val gender = genderList(random.nextInt(genderList.length))
         val cy = random.nextInt(collegeYear.length)
         val year =  collegeYear(cy)
-        val age = if (insert_fault) studentId else random.nextInt(6) + 15+ cy*2 //Each year has a consistent average age (17,19,21,23)
+        val insert_fault = shouldInjectFault(year)
+        val age = if (insert_fault){
+          println("Injecting")
+          studentId }else random.nextInt(6) + 15+ cy*2 //Each year has a consistent average age (17,19,21,23)
         val str = s"$studentId,$major,$gender,$year,$age"
         str
       }.toIterator
